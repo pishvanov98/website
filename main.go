f4a25714bb8af65d3d9360b6ad5875db4e12e466
nikita <nikita@aveldent.ru>
2023-10-12 15:12:41 +0300

    Подключение mysql, работа с бд, добавление и перебор записей

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"net/http"
 )
@@ -13,20 +15,12 @@ type User struct {
 	Avg_grades, Happiness float64
 	Hobbies               []string
 }
-
-func (u User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and he has money"+
-		"equal: %d", u.Name, u.Age, u.Money)
-}
-
-func (u *User) setNewName(newName string) {
-	u.Name = newName
+type UserDb struct {
+	Name string `json:"name"`
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
 	bob := User{"Bob", 25, -50, 4.2, 0.8, []string{"Football", "Skate", "Dance"}}
-	//bob.setNewName("alex")
-	//fmt.Fprintf(w, bob.getAllInfo())
 
 	tmpl, _ := template.ParseFiles("templates/home_page.html")
 	tmpl.Execute(w, bob)
@@ -44,8 +38,39 @@ func handleRequest() {
 
 func main() {
 
-	//bob := User{name: "Bob", age: "25", money: -50, avg_grades: 4.2, happiness: 0.8}
-	//bob := User{"Bob", 25, -50, 4.2, 0.8}
-	handleRequest()
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	////Установка данных
+	//insert, err := db.Query("INSERT INTO `users` (`name`) VALUES('Bobbys')")
+	//
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer insert.Close()
+
+	fmt.Println("Подключено к MySQL")
+
+	res, err := db.Query("SELECT `name` FROM `users`")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for res.Next() {
+		var user UserDb
+		err = res.Scan(&user.Name) //запись
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Имя: " + user.Name)
+	}
+
+	//handleRequest()
 
 }
